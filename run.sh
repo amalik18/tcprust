fed5ca75650fa1c7fabaebebf00e96ec77bbd9d2
amalik18 <47756654+amalik18@users.noreply.github.com>
2023-08-28 01:38:56 -0400

    Capture proto and flags of the TCP packet

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cargo build --release
-sudo setcap cap_net_admin=eip $CARGO_TARGET_DIR/release/tcprust
-$CARGO_TARGET_DIR/release/tcprust &
+sudo setcap cap_net_admin=eip "$CARGO_TARGET_DIR"/release/tcprust
+"$CARGO_TARGET_DIR"/release/tcprust &
+pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
-fg
+trap kill $pid TERM
+wait $pid
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use std::io;
 fn main() -> io::Result<()>{
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buffer = [0u8; 1504];
-    let nbytes = nic.recv(&mut buffer[..])?;
-    eprintln!("read {} bytes: {:x?}", nbytes, &buffer[..nbytes]);
+    loop {
+        let nbytes = nic.recv(&mut buffer[..])?;
+        let flags = u16::from_be_bytes([buffer[0], buffer[1]]);
+        let proto = u16::from_be_bytes([buffer[2], buffer[3]]);
+        eprintln!("read {} bytes: {:x?}", nbytes - 4, &buffer[4..nbytes]);
+    }
     Ok(())
 }

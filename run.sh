451e0de29412e3e52a5548773f2d658e82632d5c
amalik18 <47756654+amalik18@users.noreply.github.com>
2023-08-30 01:39:27 -0400

    feat: Add struct for connection State and connection Pairs

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 cargo build --release
+# Don't continue if complilation fails
+ext=$?
+if [[ $ext -ne 0 ]]; then
+  exit $ext
+fi
 sudo setcap cap_net_admin=eip "$CARGO_TARGET_DIR"/release/tcprust
 "$CARGO_TARGET_DIR"/release/tcprust &
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
-trap kill $pid TERM
+trap 'kill $pid' INT TERM
 wait $pid
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,60 @@
+use std::collections::HashMap;
 use std::io;
+use std::net::Ipv4Addr;
 
-fn main() -> io::Result<()>{
+mod tcp;
+
+struct TcpState {}
+
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
+struct TcpPair {
+    // (IPaddr, port#)
+    src: (Ipv4Addr, u16),
+    dest: (Ipv4Addr, u16),
+}
+
+fn main() -> io::Result<()> {
+    let mut connections: HashMap<TcpPair, tcp::State> = Default::default();
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buffer = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buffer[..])?;
-        let flags = u16::from_be_bytes([buffer[0], buffer[1]]);
-        let proto = u16::from_be_bytes([buffer[2], buffer[3]]);
-        eprintln!("read {} bytes: {:x?}", nbytes - 4, &buffer[4..nbytes]);
+        let eth_flags = u16::from_be_bytes([buffer[0], buffer[1]]);
+        let eth_proto = u16::from_be_bytes([buffer[2], buffer[3]]);
+        if eth_proto != 0x0800 {
+            // [Link to Proto number](https://www.iana.org/assignments/ieee-802-numbers/ieee-802-numbers.xhtml)
+            // we don't want anything but IPv4 packets
+            continue;
+        };
+        match etherparse::Ipv4HeaderSlice::from_slice(&buffer[4..nbytes]) {
+            Ok(ip_packet) => {
+                let src_addr = ip_packet.source_addr();
+                let dest_addr = ip_packet.destination_addr();
+                if ip_packet.protocol() != 0x06 {
+                    // We don't want anything but TCP packets
+                    continue;
+                }
+                match etherparse::TcpHeaderSlice::from_slice(&buffer[4 + ip_packet.slice().len()..])
+                {
+                    Ok(tcp_segment) => {
+                        let data = 4 + ip_packet.slice().len() + tcp_segment.slice().len();
+                        connections
+                            .entry(TcpPair {
+                                src: (src_addr, tcp_segment.source_port()),
+                                dest: (dest_addr, tcp_segment.destination_port()),
+                            })
+                            .or_default()
+                            .on_packet(ip_packet, tcp_segment, &buffer[data..]);
+                    }
+                    Err(e) => {
+                        eprintln!("Ignoring this tcp segment {:?}", e);
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("Ignoring this ip packet {:?}", e);
+            }
+        }
     }
     Ok(())
 }
